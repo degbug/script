2614a3ff4472e875067f6171477bacfd59906624
jianhualei <jianhua.lei@ximalaya.com>
2019-08-28 14:11:12 +0800

    脚本调整

--- mp.weixin.qq.com/script.js
+++ mp.weixin.qq.com/script.js
@@ -1,34 +1,54 @@
-var pres = $('pre');
-var preL = pres.length;
-var i = preL;
-for (i = 0; i < preL; i++) {
-    var codes = $(pres[i]).children('code');
-    var codeL = codes.length;
-    $(pres[i]).removeClass();
-    if (codeL > 0) {
-        var text = '';
-        var j = 0;
-        for (j = 0; j < codeL; j++) {
-            if($(codes[j]).html().indexOf('<br')>=0){
-                var replacedHtml = $(codes[j]).html().replace(/<br.*?>/g, '\n');
-                $(codes[j]).html(replacedHtml);
-            }
-            text += codes[j].textContent + '\n';
+
+showImg();
+formatCode();
+
+function showImg() {
+
+    $('img').each(function () {
+        var dataSrc = $(this).attr('data-src');
+
+        if (dataSrc) {
+            $(this).attr('src', dataSrc);
+            $(this).attr('_src', dataSrc);
+            $(this).removeAttr('data-src');
+
         }
-        text = text.replace(/</g, '&lt;').replace(/>/g, '&gt;')
+    });
+}
 
-        $(pres[i]).html(text);
-    } else {
-        var codes = $(pres[i]).html().replace(/<br.*?>/g, '\n');
-        $(pres[i]).html(codes)
-    }
+function formatCode() {
+    var pres = $('pre');
+    var preL = pres.length;
+    var i = preL;
+    for (i = 0; i < preL; i++) {
+        var codes = $(pres[i]).children('code');
+        var codeL = codes.length;
+        $(pres[i]).removeClass();
+        if (codeL > 0) {
+            var text = '';
+            var j = 0;
+            for (j = 0; j < codeL; j++) {
+                if ($(codes[j]).html().indexOf('<br') >= 0) {
+                    var replacedHtml = $(codes[j]).html().replace(/<br.*?>/g, '\n');
+                    $(codes[j]).html(replacedHtml);
+                }
+                text += codes[j].textContent + '\n';
+            }
+            text = text.replace(/</g, '&lt;').replace(/>/g, '&gt;')
 
-    if($($('pre')[i]).parent()[0]){
-        var hasNotText = $($('pre')[i]).prev().text() === "" || $($('pre')[i]).prev().text() === null;
-        if ('SECTION' === $($('pre')[i]).parent()[0].tagName && hasNotText) {
-            $(pres[i]).parent().removeClass();
-            $(pres[i]).parent().children('ul').remove();
-            $(pres[i]).parent().replaceWith($(pres[i]));
+            $(pres[i]).html(text);
+        } else {
+            var codes = $(pres[i]).html().replace(/<br.*?>/g, '\n');
+            $(pres[i]).html(codes)
+        }
+
+        if ($($('pre')[i]).parent()[0]) {
+            var hasNotText = $($('pre')[i]).prev().text() === "" || $($('pre')[i]).prev().text() === null;
+            if ('SECTION' === $($('pre')[i]).parent()[0].tagName && hasNotText) {
+                $(pres[i]).parent().removeClass();
+                $(pres[i]).parent().children('ul').remove();
+                $(pres[i]).parent().replaceWith($(pres[i]));
+            }
         }
     }
 }
