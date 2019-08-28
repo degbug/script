3bf707b4fdc3ea1b6a52e9ece551d36dbcff5138
jianhualei <jianhua.lei@ximalaya.com>
2019-08-28 10:22:59 +0800

    csdn规则

--- blog.csdn.net/script.js
+++ blog.csdn.net/script.js
@@ -1,1 +1,32 @@
-$('code').next('.pre-numbering').remove()
+$('code').next('.pre-numbering').remove();
+var pres = $('pre');
+var preL = pres.length;
+var i = preL;
+for (i = 0; i < preL; i++) {
+    var codes = $(pres[i]).children('code');
+    var codeL = codes.length;
+    $(pres[i]).removeClass();
+    if (codeL > 0) {
+        var text = '';
+        var j = 0;
+        for (j = 0; j < codeL; j++) {
+            if($(codes[j]).html().indexOf('<br')>=0){
+                var replacedHtml = $(codes[j]).html().replace(/<br.*?>/g, '\n');
+                $(codes[j]).html(replacedHtml);
+            }
+            text += codes[j].textContent + '\n';
+        }
+        text = text.replace(/</g, '&lt;').replace(/>/g, '&gt;')
+
+        $(pres[i]).html(text);
+    } else {
+        var codes = $(pres[i]).html().replace(/<br>/g, '\n');
+        $(pres[i]).html(codes)
+    }
+
+    if ('SECTION' === $($('pre')[i]).parent()[0].tagName) {
+        $(pres[i]).parent().removeClass();
+        $(pres[i]).parent().children('ul').remove();
+        $(pres[i]).parent().replaceWith($(pres[i]));
+    }
+}
